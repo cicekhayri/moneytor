8ceb77523a4519fd79002b23c144f39aeccad3fa
Hayri <hayri@hoshilab.com>
2014-10-23 23:10:36 +0200

    sessions create action test RED

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe SessionsController, :type => :controller do
       expect(response.status).to eq(200)
     end
   end
+
+  describe "POST #create" do
+    it "should create login session" do
+      user = create(:user, password: "pass323@", password_confirmation: "pass323@")
+      post :create, email: user.email, password: "pass323@"
+      expect(session[:user_id]).to eq(user.id)
+    end
+  end
 end
