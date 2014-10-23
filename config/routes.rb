abd1eb39c9d32c0c24b6325945a408739aef0452
Hayri <hayri@hoshilab.com>
2014-10-23 23:21:04 +0200

    sessions destroy action test GREEN

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:create]
   get 'login', to: "sessions#new"
- 
+  get 'logout', to: "sessions#destroy" 
   # The priority is based upon order of creation: first created -> highest priority.
 
   # See how all your routes lay out with "rake routes".
