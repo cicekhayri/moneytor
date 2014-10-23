a4655a3997bb5dd59fddbe902bdeb0b4fd798efd
Hayri <hayri@hoshilab.com>
2014-10-23 23:27:54 +0200

    user email uniquness test RED

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe User, type: :model do
       user.email = nil
       expect(user).not_to be_valid
     end
+
+    it "should have a unique email" do
+      new_user = FactoryGirl.build(:user, email: "user1@example.com")
+      older_user = FactoryGirl.create(:user)
+      new_user.email = older_user.email
+      new_user.should_not be_valid
+    end
   end
 end
