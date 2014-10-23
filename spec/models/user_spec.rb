221003b80086f2674e557ced93ef5c7484f0b181
Hayri <hayri@hoshilab.com>
2014-10-23 23:25:21 +0200

    user model test valid email GREEN

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,1 +1,11 @@
+require 'rails_helper'
 
+RSpec.describe User, type: :model do
+  describe "Validations" do
+    it "should have valid email" do
+      user = FactoryGirl.build(:user)
+      user.email = nil
+      expect(user).not_to be_valid
+    end
+  end
+end
