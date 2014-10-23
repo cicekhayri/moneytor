ba6e2c8e61c35a30f44c808bad25e9c5f8cedeec
Hayri <hayri@hoshilab.com>
2014-10-23 23:07:46 +0200

    sessions new action test GREEN

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   
   resources :users, only: [:new, :create]
+  get 'login', to: "sessions#new"
   # The priority is based upon order of creation: first created -> highest priority.
 
   # See how all your routes lay out with "rake routes".
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, :type => :controller do
-
+  describe "GET #new" do
+    it "should have status 200" do
+      get :new
+      expect(response.status).to eq(200)
+    end
+  end
 end
