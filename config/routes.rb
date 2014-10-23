1fcfcc8b3da626943982f26046be066e8738a9ac
Hayri <hayri@hoshilab.com>
2014-10-23 22:24:25 +0200

    user new action GREEN

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  
+  resources :users, only: [:new]
   # The priority is based upon order of creation: first created -> highest priority.
+
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
