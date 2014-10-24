1a5a5a9ceebb347e6aa0a1aa016006143a730ad1
Hayri <hayri@hoshilab.com>
2014-10-24 10:55:40 +0200

    category create action test GREEN

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,20 @@ class CategoriesController < ApplicationController
   def new
   end
 
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = "Category saved successfully"
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+  
+  private
+  def category_params
+    params.require(:category).permit(:title)
+  end
+
 end
--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe CategoriesController, :type => :controller do
   describe "#create action" do
     it "should response with 200" do
       post :create, category: Category.new.attributes
-      expect(response.status).to eq(200)
+      expect(response.status).to eq(302)
     end
   end
 end
