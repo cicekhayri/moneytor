18b54f711bd4beabdf4817fea8a9ef27f0054a01
Hayri <hayri@hoshilab.com>
2014-10-24 10:40:18 +0200

    user signup and login, logout works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to users_path
+      redirect_to root_path
     else
       render :new
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :get_user, only: [:show, :update, :destroy]
+  before_filter :get_user, only: [:edit, :show, :update, :destroy]
   def new
     @user = User.new
   end
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       flash[:notice] = "Thanks for signing up"
       redirect_to root_url
     else
