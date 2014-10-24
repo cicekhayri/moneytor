776014d99da5b51b35d295ca1cce52f091ee9875
Hayri <hayri@hoshilab.com>
2014-10-24 23:49:48 +0200

    sign_up_user method implemented

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,3 +1,9 @@
 class UserNotifier < ActionMailer::Base
   default from: "from@example.com"
+
+  def sign_up_user(user)
+    @user = user
+
+    mail(to: @user.email, subject: 'Thanks for signing up for our amazing app')
+  end
 end
