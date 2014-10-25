15e3ae0f6f9ee00a20358a4f191dec174a86c4b0
Hayri <hayri@hoshilab.com>
2014-10-25 17:16:44 +0200

    Purchases test RED

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141025135935) do
+ActiveRecord::Schema.define(version: 20141025151251) do
 
   create_table "categories", force: true do |t|
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "purchases", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "recipes", force: true do |t|
--- spec/controllers/purchases_controller_spec.rb
+++ spec/controllers/purchases_controller_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe PurchasesController, :type => :controller do
+  before do 
+    @purchase = FactoryGirl.create(:purchase)
+  end
 
+  describe "#new action" do
+    it "should have status 200" do
+      get :new
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "#index action" do
+    it "should response with status 200" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+
+    it "should render the index view template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "#create action" do
+    it "should response with status 200" do
+      post :create, purchase: Purchase.new.attributes
+      expect(response.status).to eq(200)
+    end
+
+    it "should save to the datebase if successfull" do
+      post :create, purchase: { product: "Shoes", purchase_date: "2014-11-11", amount: "200", store_name: "E-Store"}
+      purchase = Purchase.last
+      expect(purchase.product).to eq("Shoes")
+    end
+
+    it "should not save to the database if not successfull" do
+      post :create, purchase: { product: "" }
+      expect(response).to render_template :new
+    end
+  end
+  
+  describe "#show action" do
+    it "should response with status 200" do
+      get :show, id: @purchase.id
+      expect(response).to render_template :show
+    end
+  end
 end
