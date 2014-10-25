e5833a86a93c5f2d4d43760991412a4a1497466a
Hayri <hayri@hoshilab.com>
2014-10-25 16:41:51 +0200

    recipe create action test RED

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe RecipesController, :type => :controller do
+  before do 
+    @recipe = FactoryGirl.create(:recipe)
+  end
+
   describe "#new action" do
     it "should have status 200" do
       get :new
@@ -17,6 +21,13 @@ RSpec.describe RecipesController, :type => :controller do
     it "should render the index view template" do
       get :index
       expect(response).to render_template("index")
+    end
+  end
+
+  describe "#create action" do
+    it "should response with status 200" do
+      post :create, recipe: Recipe.new.attributes
+      expect(response).to eq(200)
     end
   end
 end
