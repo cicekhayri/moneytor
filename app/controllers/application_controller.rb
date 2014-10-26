29d00fb3b7018fa1db518e89d999d2491a826aac
Hayri <hayri@hoshilab.com>
2014-10-26 17:56:30 +0100

    previous and next month working yaayyy :)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :is_admin
   helper_method :get_current_month_purchases
+  helper_method :next_month
 
   def current_user
     if session[:user_id]
@@ -20,6 +21,15 @@ class ApplicationController < ActionController::Base
   end
 
   def get_current_month_purchases
-    Purchase.where(user_id: current_user.id).where("purchase_date BETWEEN ? AND ?", Date.today.beginning_of_month, Date.today.end_of_month).sum(:amount)
+    if next_month != nil
+      Purchase.where(user_id: current_user.id).where("purchase_date BETWEEN ? AND ?", @beginning_current.beginning_of_month, @beginning_current.end_of_month).sum(:amount)
+    end
+  end
+  
+  def next_month
+    @beginning_current = params[:beginning_of_month].try(:to_date) || Date.current.beginning_of_month
+    @date_range = (@beginning_current.beginning_of_month..@beginning_current.end_of_month) 
+    @beginning_next     = (@beginning_current + 1.month).beginning_of_month.to_s
+    @beginning_previous = (@beginning_current - 1.month).beginning_of_month.to_s      
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-
+    next_month
   end
 end
