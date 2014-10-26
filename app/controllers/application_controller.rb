ce54054d45549f7de70a6c13bd8e71e01e5fea36
Hayri <hayri@hoshilab.com>
2014-10-26 12:36:45 +0100

    extracted the purchases of month from view to application controller helper_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
   helper_method :is_admin
+  helper_method :get_current_month_purchases
 
   def current_user
     if session[:user_id]
@@ -16,5 +17,9 @@ class ApplicationController < ActionController::Base
     if !current_user
       redirect_to root_url
     end
+  end
+
+  def get_current_month_purchases
+    @purchase_of_month = Purchase.where(user_id: current_user.id).where("purchase_date BETWEEN ? AND ?", Date.today.beginning_of_month, Date.today.end_of_month).sum(:amount)
   end
 end
