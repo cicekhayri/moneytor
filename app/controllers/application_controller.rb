692b0d94ca3784a3c41f733577221b97a928634a
Hayri <hayri@hoshilab.com>
2014-10-27 23:48:51 +0100

    user working properly :)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,14 +11,14 @@ class ApplicationController < ActionController::Base
   helper_method :get_month
   helper_method :pie_chart_current_month
 
-  def current_user
+  def current_user 
     if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
+      @current_user ||= User.find(session[:user_id])  
     end
   end
 
   def authorize
-    if current_user != User.find(params[:id])
+    if !current_user
       redirect_to root_url
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,9 +48,11 @@ class UsersController < ApplicationController
   private
   def get_user
     @user = User.find(params[:id])
+    redirect_to root_path unless current_user == @user
   end
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
-end
+
+ end
