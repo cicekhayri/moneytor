28e61796503e5dea053dc4c9e8784713bcc32be2
Hayri <hayri@hoshilab.com>
2014-10-30 17:03:35 +0100

    minor change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,15 +35,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_protocol => 'http',
-    :s3_credentials => {
-      :bucket => ENV["BUCKET_NAME" ],
-      :access_key_id => ENV["ACCESS_KEY_ID" ],
-      :secret_access_key => ENV["SECRET_ACCESS_KEY" ]
-    },
-    url: ":s3_domain_url",
-    path: "/:class/:attachment/:id_partition/:style/:filename"
-  }
 end
