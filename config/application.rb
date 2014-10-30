56b2d2bfcf8524d56a1e3289522d4feb73be228f
Hayri <hayri@hoshilab.com>
2014-10-30 12:08:01 +0100

    fix date error translation

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,9 @@ module Moneytor
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.serve_static_assets = true
+    #config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ Rails.application.routes.draw do
 
   get 'admin_index', to: "admin#index", as: 'admin_index'
 
-  get 'previous', to: "welcome#index"
-  get 'next', to: "welcome#index"
+
   get 'show_by_category', to: 'welcome#show_by_category'
   
   get 'login', to: "sessions#new"
@@ -22,6 +21,8 @@ Rails.application.routes.draw do
     resources :users
     resources :purchases
     resources :categories
+    get 'previous', to: "welcome#index"
+    get 'next', to: "welcome#index"
   end
   get '/:locale' => 'welcome#index'
 end
